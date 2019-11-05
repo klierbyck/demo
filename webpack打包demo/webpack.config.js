d4ee8af6c4c0b5f0e1f96743bea7db22c9b4cb07
klierbyck <740411080@qq.com>
2019-11-05 11:40:12 +0800

    Update webpack.config.js

--- webpack打包demo/webpack.config.js
+++ webpack打包demo/webpack.config.js
@@ -2,12 +2,13 @@ const path = require("path");
 const webpack = require("webpack");
 const htmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const extractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     //提示错误在哪一行的工具插件
     devtool: "inline-source-map",
     //程序入口文件
-    entry : "./src/index.js",
+    entry: "./src/index.js",
     /* 
     //多文件入口时
     entry: {
@@ -20,40 +21,51 @@ module.exports = {
     },
     */
     //处理后输入文件
-    output : {
-        path : path.resolve(__dirname + "/dist"),
-        filename : "bundle-[hash].js"
+    output: {
+        path: path.resolve(__dirname + "/dist"),
+        filename: "bundle-[hash].js"
     },
     //服务器默认开启目录，文件更改会实时刷新页面
-    devServer : {
-        contentBase : "./dist",//本地服务器所加载的页面所在的目录
+    devServer: {
+        contentBase: "./dist",//本地服务器所加载的页面所在的目录
         historyApiFallback: true,//不跳转
         inline: true,
         hot: true
     },
     //使用loader处理文件
-    module : {
+    module: {
         //1.0为loaders
-        rules : [
+        rules: [
             //css模块化配置
             {
                 test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    }, 
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true
+                use: extractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                url: false
+                            }
                         }
-                    }
-                ]
+                    ]
+                })
+                /*   use: [
+                      {
+                          loader: "style-loader"
+                      }, 
+                      {
+                          loader: "css-loader",
+                          options: {
+                              modules: true
+                          }
+                      }
+                  ] */
             },
             //less处理
             {
-                test : /\.less$/,
-                use : ["style-loader","css-loader","less-loader"]
+                test: /\.less$/,
+                use: ["style-loader", "css-loader", "less-loader"]
             },
             //图片处理
             {
@@ -62,14 +74,14 @@ module.exports = {
             },
             //es6
             {
-                test:/(\.jsx|\.js)$/,
+                test: /(\.jsx|\.js)$/,
                 //不解析文件目录
-                exclude:/node_modules/,
-                loader:'babel-loader',
+                exclude: /node_modules/,
+                loader: 'babel-loader',
                 //1.0为query
-                options:{
+                options: {
                     //转换es代码及jsx代码
-                    presets:['es2015','react','stage-2']
+                    presets: ['es2015', 'react', 'stage-2']
                 }
             }
         ]
@@ -77,16 +89,18 @@ module.exports = {
     plugins: [
         //自动生成html页面显示文件title
         new htmlWebpackPlugin({
-            title : "新信息"
+            title: "新信息"
         }),
         new CleanWebpackPlugin(
             ['dist'],　 //匹配删除的文件
             {
-                root:  __dirname,//根目录
+                root: __dirname,//根目录
                 verbose: true,//开启在控制台输出信息
                 dry: false//启用删除文件
             }
-        )
-        
+        ),
+        new extractTextPlugin({
+            filename: 'focus.index.css'
+        })
     ]
 }
